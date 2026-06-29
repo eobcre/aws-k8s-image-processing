aa21de33cacff918abc445deb2d60257e1a6394d
Emmy Manning <emswoo@EM.local>
2026-06-29 17:40:04 -0400

    feature: add uploader function to upload to s3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,50 @@
-import Polaroid from "./components/Polaroid";
+import { useState } from "react";
+
 import Uploader from "./components/Uploader";
+import Polaroid from "./components/Polaroid";
+
+import type { UploadedImage } from "./type";
 
 const App = () => {
+  const [uploadedImage, setUploadedImage] = useState<UploadedImage | null>(null);
+
+  // upload
+  const onUpload = async (selectedFile: File) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/upload`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!res.ok) {
+        throw new Error("Upload failed...");
+      }
+
+      const data = await res.json();
+
+      setUploadedImage({
+        name: selectedFile.name,
+        key: data.key,
+        size: "5MB",
+        uploadedAt: "10:30 PM",
+        caption: "I had so much fun today!",
+        tags: data.tags ?? ["summer", "beach", "vacation"],
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center bg-linear-to-br from-stone-100 via-rose-50 to-orange-100 px-4 py-10 min-h-screen">
       <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-10 w-full max-w-5xl ">
         {/* left */}
-        <Uploader />
+        <Uploader onUpload={onUpload} />
         {/* right */}
-        <Polaroid />
+        <Polaroid uploadedImage={uploadedImage} />
       </div>
     </div>
   );
