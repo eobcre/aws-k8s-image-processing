25ca866c6b8361787d38f47ecab4b2a205c287c7
Emmy Manning <emswoo@EM.local>
2026-06-28 20:51:07 -0400

    feature: add uploader to s3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,56 @@
+import { useState } from "react";
 import ImageOutlineIcon from "@iconify-react/material-symbols/image-outline";
 
 const App = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const onUpload = async (selectedFile: File) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/upload`, {
+        method: "POST",
+        body: formData,
+      });
+
+      // console.log("Uploaded to S3!!!");
+
+      if (!res.ok) {
+        throw new Error("Upload failed");
+      }
+
+      const text = await res.text();
+      console.log(text);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex flex-col justify-center items-center min-h-screen">
       <div className="bg-white text-center border-2 border-dashed border-gray-300 rounded-2xl shadow-sm p-6 w-full max-w-md">
         <div className="flex flex-col justify-center items-center gap-3">
           <div className="flex justify-center items-center bg-gray-100 rounded-full w-14 h-14">
             <ImageOutlineIcon className="text-gray-600 w-8" />
           </div>
-
           <div>
             <p className="text-sm font-medium text-gray-700">Upload your image</p>
             <p className="text-xs text-gray-500 mt-1">PNG, JPG, or JPEG up to 5MB</p>
           </div>
-
           <label className="bg-black hover:bg-gray-800 text-white text-sm font-medium rounded-lg cursor-pointer mt-3 px-4 py-2">
             Choose File
-            <input type="file" accept="image/png, image/jpeg" className="hidden" />
+            <input
+              type="file"
+              accept="image/png, image/jpeg"
+              className="hidden"
+              onChange={async (e) => {
+                const selectedFile = e.target.files?.[0];
+                if (!selectedFile) return;
+                setFile(selectedFile);
+                await onUpload(selectedFile);
+              }}
+            />
           </label>
         </div>
       </div>
