bb59c1f0bd551603198b554cbd4ce73ae8e9a45e
Emmy Manning <emswoo@EM.local>
2026-06-29 21:41:03 -0400

    feature: add generate api call

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,14 @@ import type { UploadedImage } from "./type";
 
 const App = () => {
   const [uploadedImage, setUploadedImage] = useState<UploadedImage | null>(null);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const loadingMessages = ["Uploading image...", "Analyzing image...", "Generating AI caption...", "Creating hashtags...", "Finalizing results..."];
 
   // upload
   const onUpload = async (selectedFile: File) => {
     try {
+      // * * * s3 upload api
       // send request for file
       const formData = new FormData();
       // add file details
@@ -29,9 +33,22 @@ const App = () => {
       // get data
       const data = await res.json();
 
+      // fall back image
       const previewUrl = URL.createObjectURL(selectedFile);
-
       // console.log("data:", data);
+
+      // * * * caption and tags generate api
+      const resGenerate = await fetch(`${import.meta.env.VITE_API_URL}/generate`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          key: data.key,
+        }),
+      });
+
+      const dataGenerate = await resGenerate.json();
 
       // save in state to display
       setUploadedImage({
@@ -39,10 +56,10 @@ const App = () => {
         key: data.key,
         imageUrl: data.imageUrl || previewUrl,
         // imageUrl: data.imageUrl,
-        size: "5MB",
-        uploadedAt: "10:30 PM",
-        caption: "I had so much fun today!",
-        tags: data.tags ?? ["summer", "beach", "vacation"],
+        size: selectedFile.size,
+        uploadedAt: selectedFile.lastModified,
+        caption:  dataGenerate.caption ?? "I had so much fun today!",
+        tags: dataGenerate.tags ?? ["summer", "beach", "vacation"],
       });
     } catch (error) {
       console.error(error);
@@ -53,7 +70,7 @@ const App = () => {
     <div className="flex justify-center items-center bg-linear-to-br from-stone-100 via-rose-50 to-orange-100 px-4 py-10 min-h-screen">
       <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-10 w-full max-w-5xl ">
         {/* left */}
-        <Uploader onUpload={onUpload} />
+        <Uploader onUpload={onUpload} isGenerating={isGenerating} loadingMessages={loadingMessages} />
         {/* right */}
         <Polaroid uploadedImage={uploadedImage} />
       </div>
