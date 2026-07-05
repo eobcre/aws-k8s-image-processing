d3074634a20c4d39e33b68597e1d697b62e3a049
Emmy Manning <emswoo@EM.local>
2026-07-05 18:01:56 -0400

    feature: add private subnets

--- infrastructure/vpc/main.tf
+++ infrastructure/vpc/main.tf
@@ -48,6 +48,22 @@ resource "aws_subnet" "public" {
 }
 
 #################################
+# Private Subnets
+#################################
+
+resource "aws_subnet" "private" {
+  count = 2
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.project_name}-private-subnet-${count.index + 1}"
+  }
+}
+
+#################################
 # Internet Gateway
 #################################
 
