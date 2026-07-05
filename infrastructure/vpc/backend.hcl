be7fe28d6aff0cae89aee788d3cacacbcc0424af
Emmy Manning <emswoo@EM.local>
2026-07-05 15:23:57 -0400

    feature: add initial vpc configuration

---
+++ infrastructure/vpc/backend.hcl
@@ -0,0 +1,3 @@
+bucket = "k8s-terraform-tfstate-bucket"
+key    = "vpc/terraform.tfstate"
+region = "us-east-1"
---
+++ infrastructure/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones for public subnets"
+  type        = list(string)
+}
