4fd727415865e6d9eff55801cecae1994a5d00c3
Emmy Manning <emswoo@EM.local>
2026-07-07 12:43:40 -0400

    chore: add base url to cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.BASE_URL,
+    // origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "OPTIONS"],
   }),
 );
